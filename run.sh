90d192592ff6680c7eea7e28d4c0067c50253358
zhi-rong <jordan990301@gmail.com>
2022-12-16 09:29:06 +0800

    add run

---
+++ run.sh
@@ -0,0 +1,10 @@
+rm -rf ./task1/submission.csv
+rm -rf ./task2/submission.csv
+rm -rf ./task3/submission.csv
+rm -rf ./task4/submission.csv
+
+python3 ./task1/main.py
+python3 ./task2/main.py
+python3 ./task3/main.py
+python3 ./task4/main.py
+
